636f551b7b51bf41ed2fa7b20e0a5ad0ce144529
AmirSaber Sharifi <amir.s.sharifi@gmail.com>
2017-07-10 10:36:11 -0400

    remove tmp from git

--- tmp/src/components/card/actions/card-actions.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'realster-card-actions',
-  template: `
-    <ng-content></ng-content>
-  `,
-})
-export class CardActionsComponent { }
