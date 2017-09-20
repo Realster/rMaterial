683c0f657b24c79c6c467d0bffbd10128d922eda
smlee <sangmlee23@gmail.com>
2017-09-20 11:08:45 -0400

    dialog styles and outside data support

--- dist/src/components/card/image/card-image.component.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CardImageComponent {
-    src: any;
-}
--- dist/src/services/portal.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentFactoryResolver } from '@angular/core';
-export declare class PortalService {
-    private resolver;
-    private component;
-    constructor(resolver: ComponentFactoryResolver);
-    Component: any;
-}
